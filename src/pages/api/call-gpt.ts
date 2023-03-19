3bd242f60ef84e4148da19cdfb4c8b367b98fd62
vipansh <vipanshthakur@gmail.com>
2023-03-20 01:13:15 +0530

    remove caching

--- src/pages/api/call-gpt.ts
+++ src/pages/api/call-gpt.ts
@@ -8,7 +8,6 @@ const configuration = new Configuration({
 export const openai = new OpenAIApi(configuration);
 
 // Create an in-memory cache using a Map
-const cache = new Map();
 
 export default async function callGpt(
   req: NextApiRequest,
@@ -20,13 +19,6 @@ export default async function callGpt(
 
   try {
     const { contextText, query } = req.body;
-
-    // Check if the response is already cached
-    const cacheKey = `${query}`;
-    if (cache.has(cacheKey)) {
-      console.log("Returning cached response");
-      return res.json({ result: cache.get(cacheKey) });
-    }
 
     const prompt = `
     As a knowledgeable React developer who enjoys assisting others, please prioritize answering the following question using the information provided in the given React documentation sections. Make sure your response is in markdown format and includes relevant code snippets when possible. You may also incorporate your own knowledge on the topic, but prioritize the contextText as it contains the latest information. If the question is unrelated to programming or the answer cannot be found in the documentation, simply respond with "Sorry, I don't know how to help with that."
@@ -50,7 +42,6 @@ export default async function callGpt(
     const result = response?.data?.choices?.[0]?.message?.content?.trim();
 
     // Cache the response for future use
-    cache.set(cacheKey, result);
 
     // Return the extracted data as a JSON object
     return res.json({ result });
